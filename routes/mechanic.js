e7b19244084dbec5954d811be6f704210ec49e1e
Usman <Usmanahsan650@gmail.com>
2022-05-08 13:34:50 +0500

    Backend Altered for mechanic portal|generating report,login and some other apis

--- routes/mechanic.js
+++ routes/mechanic.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 const mechanicController = require('../controllers/mechanicController');
 
 router.get('/mechanics', mechanicController.mechanic_list);
 router.get('/mechanics/:location', mechanicController.mechanic_by_location);
 router.get('/certified_vehicles', mechanicController.mechanic_certified_vehicles);
-
+router.post('/generateReport',mechanicController.generate_Report);
+router.post('/login',mechanicController.login);
+router.get('/mechanic/info',mechanicController.MechanicInfo);
 module.exports = router;
